6f39b8d7d035366551abef73279c1a0bec67a441
한재원 <hanjo8813@gmail.com>
2021-10-05 23:26:11 +0900

    .

--- Programmers/Python/카카오_2_후보키.py
+++ Programmers/Python/카카오_2_후보키.py
@@ -1,32 +1,41 @@
 from itertools import combinations
 
 def solution(relation):
-    ck = []
-    combi =[]
-    column = len(relation[0])
+    keys = []
+    candidates =[]
+    length = len(relation[0])
+
     # 열들의 모든 조합 찾기
-    for i in range(1, column):
-        combi.extend(combinations(range(column),i))
+    for i in range(1, length+1):
+        candidates.extend(combinations(range(length),i))
+
     # 유일성 검사
-    for com in combi:
-        col = []
+    for candidate in candidates:
+        column = []
         for i in range(len(relation)):
-            col.append(())
-            for field in com:
-                col[i] = col[i] + (relation[i][field],)
-        if len(col) == len(set(col)) :
-            ck.append(com)
+            column.append(())
+            for column_idx in candidate:
+                column[i] += (relation[i][column_idx],)
+                print(column)
+            
+        if len(column) == len(set(column)) :
+            keys.append(candidate)
     
     # 최소성 검사
-    for i, c1 in enumerate(ck):
-        for c2 in ck[i+1:]:
+    for i, c1 in enumerate(keys):
+        for c2 in keys[i+1:]:
             c1 = set(c1)
             c2 = set(c2)
             if c1.issubset(c2) :
                 c2 = tuple(c2)
-                ck.remove(c2)
-    answer = len(ck)
+                keys.remove(c2)
+    answer = len(keys)
     return answer
+
+
+
+
+    
 
 relation = [
 ["100","ryan","music","2"],
@@ -36,14 +45,14 @@ relation = [
 ["500","muzi","music","3"],
 ["600","apeach","music","2"]
 ]
-relation2 = [
-['b','2','a','a','b'],
-['b','2','7','1','b'],
-['1','0','a','a','8'],
-['7','5','a','a','9'],
-['3','0','a','f','9'],
-]
-a = solution(relation2)
+# relation2 = [
+# ['b','2','a','a','b'],
+# ['b','2','7','1','b'],
+# ['1','0','a','a','8'],
+# ['7','5','a','a','9'],
+# ['3','0','a','f','9'],
+# ]
+a = solution(relation)
 print(a)
 
 
